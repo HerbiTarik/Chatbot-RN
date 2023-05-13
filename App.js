6128ffda4fa77c22857710b828da3ad1e3e1f11b
Herbi Tarik <herbitarik2509@gmail.com>
2023-05-13 15:06:03 +0200

    Creating simple GUI for Chatbot application in React Native

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SafeAreaView style={styles.safeAreaView}>
+            <View style={styles.container}>
+                <Text style={styles.message}>
+                    <Text>Results to be shown here</Text>
+                </Text>
+                <View style={styles.component}>
+                    <View style={styles.inputComponent}>
+                        <TextInput placeholder="Enter your question" />
+                    </View>
+                    <TouchableOpacity>
+                        <View style={styles.sendComponent}>
+                            <Text>Send</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <StatusBar style="auto" />
+            </View>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    safeAreaView: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    message: {
+        flex: 1,
+    },
+    component: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+        marginBottom: 10,
+    },
+    sendComponent: {
+        backgroundColor: 'red',
+    },
+    inputComponent: {
+        flex: 1,
+    },
+})
